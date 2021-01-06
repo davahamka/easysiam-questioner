3a1b7caa19999aee7138615b07075422fac4a22a
davahamka <davahamka512@gmail.com>
2021-01-06 20:15:07 +0700

    Update prompt

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Too excited to see your <b>grades</b>? but there is a <b>questionnaire</b> that must be filled in first. Install EASYSIAM - Questioner for those of you who want to see the grades in SIAM instantly.
 
 ## Introducing ESQ
-🚧 UNDER CONSTRUCTION 🚧
+![ESQ](https://i.ibb.co/zbBcg11/Screenshot-16.png)
 
 ## How to Install
 1. Download the ZIP file.
@@ -11,6 +11,13 @@ Too excited to see your <b>grades</b>? but there is a <b>questionnaire</b> that 
 4. Open <i>chrome://extensions/</i>
 5. Switch on Developer Mode
 6. Load unpacked with folder that was extracted.
+
+## How to Use
+1. Make sure that the chrome extension already installed in your web browser.
+2. Login SIAM and open questioner.
+3. Input value between 1-5.
+4. Input suggestion.
+5. Submit and finish.
 
 ## Credits
 🚧 UNDER CONSTRUCTION 🚧
--- content.js
+++ content.js
@@ -1,10 +1,39 @@
-let text;
-let inputElements = document.getElementsByTagName("input");
+let nilai;
+let saran;
+let isiNilai = prompt("Masukkan nilai (1-5): ", "4");
 let isiSaran = prompt("Masukkan saran: ", "Perbanyak tugas pak/bu!");
+let inputElements = document.getElementsByTagName("input");
+// let backdrop = document.createElement("div");
+// let modal = document.createElement("div");
+// let nilaiLabel = document.createElement("label");
+// nilaiLabel.appendChild(document.createTextNode("Nilai: (1-5)"));
+// let nilaiInput = document.createElement("input");
+// nilaiInput.oninput = handleNilaiInput;
+// let saranLabel = document.createElement("label");
+// saranLabel.appendChild(document.createTextNode("Saran: "));
+// let saranInput = document.createElement("textarea"); 
+// modal.appendChild(document.createElement("h2").appendChild(document.createTextNode("EZSIAM🔥")));
+// modal.appendChild(document.createElement("hr"));
+// backdrop.style.cssText =
+//   " top:0;left: 0;position: fixed;background: rgba(0, 0, 0, 0.5);width: 100%;height: 100%;";
+// modal.style.cssText =
+//   "width: 400px;padding: 20px;margin: 100px auto;background: white;border-radius:6px ;display:flex;flex-direction:column";
+// document.body.appendChild(backdrop);
+// backdrop.appendChild(modal);
+// modal.appendChild(nilaiLabel);
+// modal.appendChild(nilaiInput);
+// modal.appendChild(saranLabel);
+// modal.appendChild(saranInput);
+
 if (isiSaran == null || isiSaran == "") {
   alert("EASYSIAM terminated.");
 } else {
-  isiKuisoner(4, isiSaran);
+  isiKuisoner(isiNilai, isiSaran);
+}
+
+function handleNilaiInput(){
+  nilai = nilaiInput.value;
+  console.log(nilai)
 }
 
 function isiKuisoner(nilai, saran) {
@@ -17,10 +46,10 @@ function isiKuisoner(nilai, saran) {
   }
 
   document.getElementById("saran").appendChild(document.createTextNode(saran));
-  for(let i =0;i<inputElements.length;i++){
-    if(inputElements[i].type.toLowerCase()=='submit'){
+  for (let i = 0; i < inputElements.length; i++) {
+    if (inputElements[i].type.toLowerCase() == "submit") {
       inputElements[i].scrollIntoView({
-        behavior:'smooth'
+        behavior: "smooth",
       });
     } else {
       inputElements[i].scrollIntoView();
